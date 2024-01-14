084088d7690fbd3acfeca78fc0d18e4c3cb9588e
Alexandr Perfiliev <aleksandr.perfilev@domrf.ru>
2024-01-14 13:15:52 +0300

    crud grpc boilerplate

---
+++ cmd/grpc_server/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	desc "auth/pkg/user_v1"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"log"
+	"net"
+)
+
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedUserV1Server
+}
+
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("Note id: %d", req.GetId())
+
+	return &desc.GetResponse{}, nil
+}
+
+func (s *server) Create(ctx context.Context, req *desc.CreteRequest) (*desc.CreateResponse, error) {
+	log.Printf("Request: %d", req)
+
+	return &desc.CreateResponse{}, nil
+}
+
+func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
+	log.Printf("Request: %d", req)
+
+	return &emptypb.Empty{}, nil
+}
+
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("Request: %d", req)
+
+	return &emptypb.Empty{}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterUserV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
